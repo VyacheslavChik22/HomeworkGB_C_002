c4842835fb9c079000c6f71fa82551ff100cd63f
VyacheslavChik22 <99678206+VyacheslavChik22@users.noreply.github.com>
2023-08-12 18:21:00 +0300

    Добавил еще вариант

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -3,32 +3,64 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-int ImputNumb(string phrase)
+// int ImputNumb(string phrase)
+// {
+//     Console.Write(phrase);
+//     int num = int.Parse(Console.ReadLine()!);
+//     return num;
+// }
+
+// int num = ImputNumb("Введите любое число ");
+
+// int ThirdDigit(int num)
+// {
+//     while (num > 999)
+//     {
+//         num /= 10;
+//     }
+//     return num % 10;
+// }
+
+// bool test(int num)
+// {
+//     if (num < 100)
+//         return false;
+//     else return true;
+// }
+
+// if (test(num) != true)
+//     Console.WriteLine($"{num} Третьей цифры нет");
+// else
+//     Console.WriteLine($"Третьей цифрой числа {num} является {ThirdDigit(num)}");
+
+
+int EnterDigit(string phrase)
 {
     Console.Write(phrase);
-    int num = int.Parse(Console.ReadLine()!);
-    return num;
+    return int.Parse(Console.ReadLine()!);
 }
 
-int num = ImputNumb("Введите любое число ");
+int firstNum = EnterDigit("Введите число: ");
 
-int ThirdDigit(int num)
+int number = firstNum;
+System.Console.WriteLine(number);
+
+void ThirdDigit(int number)
 {
-    while (num > 999)
+    int result = 0;
+    if (number > 100)
     {
-        num /= 10;
+        while (true)
+        {
+            result = number / 100 % 10;
+            Console.WriteLine($"В числе {number} третья цифра {result}");
+            break;
+        }
     }
-    return num % 10;
+    else
+    {
+        Console.WriteLine("Третей цифры в этом числе здесь нет!");
+    }
 }
 
-bool test(int num)
-{
-    if (num < 100)
-        return false;
-    else return true;
-}
-
-if (test(num) != true)
-    Console.WriteLine($"{num} Третьей цифры нет");
-else
-    Console.WriteLine($"Третьей цифрой числа {num} является {ThirdDigit(num)}");
+ThirdDigit(number);
